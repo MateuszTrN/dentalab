53973da90135cf7301fbd828d7d89e44bcb01efd
Mateusz Sidorski <mateusz.sidorski@cardinality.co.uk>
2017-09-08 06:20:10 +0200

    feat(server): adds koa-static lib

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 var koa = require('koa');
 var send = require('koa-send');
+var serve = require('koa-static');
 var app = new koa();
 
-
-app.use(async ctx => {
-    console.log(ctx.path);
-    if (ctx.path === '' || ctx.path === '/') await send(ctx, 'index.html');
-    else await send(ctx, ctx.path);
-});
+app.use(serve('.'));
 
 var port = process.env.PORT || 5000;
 
 app.listen(port);
-
-console.log('App listening on port: ', port);
